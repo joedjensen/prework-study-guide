d096c4bac6ae9485e0c5da4c3157a4aa904dbd83
josephjensen <joe@cryptonumerics.com>
2022-09-18 12:52:12 -0700

    wrap existing logging in function, additional logging in scripts.js, notes on JS added to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-topics.forEach((topic) => console.log(topic));
+function listTopics() {
+    topics.forEach((topic) => console.log(topic));
+}
+
+function selectTopic() {
+    if (topics.includes(randomTopic)) {
+        console.log(''.concat("Let's study ", randomTopic, "!"))
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
